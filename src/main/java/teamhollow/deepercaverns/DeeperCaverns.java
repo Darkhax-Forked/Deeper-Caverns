9fa1b4404e9192e91c841650786d5a27009856ed
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-09-30 12:22:00 +0200

    add base files

---
+++ src/main/java/teamhollow/deepercaverns/DeeperCaverns.java
@@ -0,0 +1,13 @@
+package teamhollow.deepercaverns;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DeeperCaverns.MODID)
+public class DeeperCaverns
+{
+	public static final String MODID = "deepercaverns";
+
+	public DeeperCaverns()
+	{
+	}
+}
