e6514c0cdd978807af7fbaef5cc3332682483407
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-10-03 15:58:16 +0200

    add dark purple particles to arcane and remove unused code

--- src/main/java/teamhollow/deepercaverns/entity/ArcaneEntity.java
+++ src/main/java/teamhollow/deepercaverns/entity/ArcaneEntity.java
@@ -1,16 +1,17 @@
 package teamhollow.deepercaverns.entity;
 
-import net.minecraft.entity.CreatureEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.Particle;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.goal.LookAtGoal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
-import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 import net.minecraft.entity.ai.goal.RandomWalkingGoal;
 import net.minecraft.entity.monster.MonsterEntity;
-import net.minecraft.util.Hand;
+import net.minecraft.particles.IParticleData;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.world.World;
 
 public class ArcaneEntity extends MonsterEntity
@@ -32,38 +33,44 @@ public class ArcaneEntity extends MonsterEntity
 	}
 
 	@Override
+	public void livingTick()
+	{
+		super.livingTick();
+
+		if(world.isRemote)
+		{
+			IParticleData data = ParticleTypes.EFFECT;
+
+			int particleColor = 0x2F0076;
+			float r = (particleColor >> 16 & 255) / 255.0F;
+			float g = (particleColor >> 8 & 255) / 255.0F;
+			float b = (particleColor >> 0 & 255) / 255.0F;
+
+			for(int i = 0; i < 5; i++)
+			{
+				double velocityMultiplier = rand.nextDouble() * 0.5D;
+				double angle = rand.nextDouble() * Math.PI * 0.5D;
+				double xSpeed = Math.cos(angle);
+				double ySpeed = rand.nextDouble() * 0.5D;
+				double zSpeed = Math.sin(angle);
+				Particle particle = Minecraft.getInstance().particles.addParticle(data, posX + xSpeed * 0.1D, posY + 0.3D, posZ + zSpeed * 0.1D, xSpeed, ySpeed, zSpeed);
+
+				if(particle != null)
+				{
+					float colorOffset = 0.75F + rand.nextFloat() * 0.25F;
+
+					particle.setColor(r * colorOffset, g * colorOffset, b * colorOffset);
+					particle.multiplyVelocity((float)velocityMultiplier);
+				}
+			}
+		}
+	}
+
+	@Override
 	protected void registerAttributes()
 	{
 		super.registerAttributes();
 
 		getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20.0D);
-	}
-
-	@Override
-	public boolean canSwim()
-	{
-		return true;
-	}
-
-	class MeleeFireAttackGoal extends MeleeAttackGoal
-	{
-		public MeleeFireAttackGoal(CreatureEntity creature, double speed, boolean useLongMemory)
-		{
-			super(creature, speed, useLongMemory);
-		}
-
-		@Override
-		protected void checkAndPerformAttack(LivingEntity enemy, double distToEnemySqr)
-		{
-			double attackReachSqr = getAttackReachSqr(enemy);
-
-			if (distToEnemySqr <= attackReachSqr && attackTick <= 0)
-			{
-				attackTick = 20;
-				attacker.swingArm(Hand.MAIN_HAND);
-				attacker.attackEntityAsMob(enemy);
-				enemy.setFire(5);
-			}
-		}
 	}
 }
