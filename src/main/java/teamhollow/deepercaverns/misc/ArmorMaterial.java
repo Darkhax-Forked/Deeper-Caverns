31f7269c0aaef3ed86c1b773f7961eca459c91ac
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-10-10 15:01:59 +0200

    glowstone crystal armor

---
+++ src/main/java/teamhollow/deepercaverns/misc/ArmorMaterial.java
@@ -0,0 +1,79 @@
+package teamhollow.deepercaverns.misc;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import teamhollow.deepercaverns.DeeperCaverns;
+import teamhollow.deepercaverns.reg.ItemRegistrar;
+
+public enum ArmorMaterial implements IArmorMaterial
+{
+	//same as gold for now
+	GLOWSTONE_CRYSTAL("glowstone_crystal", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, Ingredient.fromItems(ItemRegistrar.GLOWSTONE_CRYSTAL));
+
+	private final String name;
+	private final int durability;
+	private final float toughness;
+	private final int[] damageReduction;
+	private final int enchantability;
+	private final SoundEvent soundEvent;
+	private final Ingredient repairMaterial;
+
+	ArmorMaterial(String name, int durability, int[] damageReduction, int enchantability, SoundEvent soundEvent, float toughness, Ingredient repairMaterial)
+	{
+		this.name = name;
+		this.durability = durability;
+		this.toughness = toughness;
+		this.damageReduction = damageReduction;
+		this.enchantability = enchantability;
+		this.soundEvent = soundEvent;
+		this.repairMaterial = repairMaterial;
+	}
+
+	@Override
+	public int getDurability(EquipmentSlotType slot)
+	{
+		return durability;
+	}
+
+	@Override
+	public int getDamageReductionAmount(EquipmentSlotType slot)
+	{
+		return damageReduction[slot.getIndex()];
+	}
+
+	@Override
+	public int getEnchantability()
+	{
+		return enchantability;
+	}
+
+	@Override
+	public SoundEvent getSoundEvent()
+	{
+		return soundEvent;
+	}
+
+	@Override
+	public Ingredient getRepairMaterial()
+	{
+		return repairMaterial;
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public String getName()
+	{
+		return DeeperCaverns.PREFIX + name;
+	}
+
+	@Override
+	public float getToughness()
+	{
+		return toughness;
+	}
+}
--- src/main/java/teamhollow/deepercaverns/reg/ItemRegistrar.java
+++ src/main/java/teamhollow/deepercaverns/reg/ItemRegistrar.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityType;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.SpawnEggItem;
@@ -15,6 +17,7 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ObjectHolder;
 import teamhollow.deepercaverns.DeeperCaverns;
+import teamhollow.deepercaverns.misc.ArmorMaterial;
 import teamhollow.deepercaverns.util.RegistryUtil;
 
 @EventBusSubscriber(modid=DeeperCaverns.MODID, bus=Bus.MOD)
@@ -25,6 +28,10 @@ public class ItemRegistrar
 	public static final Item ARCANE_SPAWN_EGG = RegistryUtil.injected();
 	public static final Item BRIMSTONE_POWDER = RegistryUtil.injected();
 	public static final Item GLOWSTONE_CRYSTAL = RegistryUtil.injected();
+	public static final Item GLOWSTONE_CRYSTAL_BOOTS = RegistryUtil.injected();
+	public static final Item GLOWSTONE_CRYSTAL_CHESTPLATE = RegistryUtil.injected();
+	public static final Item GLOWSTONE_CRYSTAL_HELMET = RegistryUtil.injected();
+	public static final Item GLOWSTONE_CRYSTAL_LEGGINGS = RegistryUtil.injected();
 	public static final Item GLURKER_SPAWN_EGG = RegistryUtil.injected();
 	public static final Item IGNEOUS_GOLEM_SPAWN_EGG = RegistryUtil.injected();
 	public static final Item ONYX_INGOT = RegistryUtil.injected();
@@ -46,6 +53,10 @@ public class ItemRegistrar
 		event.getRegistry().register(makeSpawnEgg(EntityRegistrar.ARCANE, 0x3C1361, 0x660066));
 		event.getRegistry().register(new Item(defaultItemProperties()).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "brimstone_powder")));
 		event.getRegistry().register(new Item(defaultItemProperties()).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "glowstone_crystal")));
+		event.getRegistry().register(new ArmorItem(ArmorMaterial.GLOWSTONE_CRYSTAL, EquipmentSlotType.FEET, defaultItemProperties()).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "glowstone_crystal_boots")));
+		event.getRegistry().register(new ArmorItem(ArmorMaterial.GLOWSTONE_CRYSTAL, EquipmentSlotType.CHEST, defaultItemProperties()).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "glowstone_crystal_chestplate")));
+		event.getRegistry().register(new ArmorItem(ArmorMaterial.GLOWSTONE_CRYSTAL, EquipmentSlotType.HEAD, defaultItemProperties()).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "glowstone_crystal_helmet")));
+		event.getRegistry().register(new ArmorItem(ArmorMaterial.GLOWSTONE_CRYSTAL, EquipmentSlotType.LEGS, defaultItemProperties()).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "glowstone_crystal_leggings")));
 		event.getRegistry().register(makeSpawnEgg(EntityRegistrar.GLURKER, 0xFF5733, 0xC70039));
 		event.getRegistry().register(makeSpawnEgg(EntityRegistrar.IGNEOUS_GOLEM, 0xEC5300, 0xFFB605));
 		event.getRegistry().register(new Item(defaultItemProperties()).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "onyx_ingot")));
