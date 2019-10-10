fb5e558e050c47d6a4bc0b8209345cdfa30573ea
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-10-10 16:50:22 +0200

    add missing armor and tool recipes

--- src/main/java/teamhollow/deepercaverns/datagen/RecipeGenerator.java
+++ src/main/java/teamhollow/deepercaverns/datagen/RecipeGenerator.java
@@ -9,6 +9,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
 import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.world.dimension.DimensionType;
@@ -86,6 +87,89 @@ public class RecipeGenerator extends RecipeProvider
 		.key('C', ItemRegistrar.CHALONITE_INGOT)
 		.key('S', Items.STICK)
 		.addCriterion("has_chalonite_ingot", hasItem(ItemRegistrar.CHALONITE_INGOT))
+		.build(consumer);
+
+		//standard tool recipes
+		addStandardToolRecipes(consumer, ItemRegistrar.GHOSTSOUL_INGOT, ItemRegistrar.GHOSTSOUL_AXE, ItemRegistrar.GHOSTSOUL_HOE, ItemRegistrar.GHOSTSOUL_PICKAXE, ItemRegistrar.GHOSTSOUL_SHOVEL, ItemRegistrar.GHOSTSOUL_SWORD);
+		addStandardToolRecipes(consumer, ItemRegistrar.GLOWSTONE_CRYSTAL, ItemRegistrar.GLOWSTONE_CRYSTAL_AXE, ItemRegistrar.GLOWSTONE_CRYSTAL_HOE, ItemRegistrar.GLOWSTONE_CRYSTAL_PICKAXE, ItemRegistrar.GLOWSTONE_CRYSTAL_SHOVEL, ItemRegistrar.GLOWSTONE_CRYSTAL_SWORD);
+		addStandardToolRecipes(consumer, ItemRegistrar.ONYX_INGOT, ItemRegistrar.ONYX_AXE, ItemRegistrar.ONYX_HOE, ItemRegistrar.ONYX_PICKAXE, ItemRegistrar.ONYX_SHOVEL, ItemRegistrar.ONYX_SWORD);
+
+		//standard armor recipes
+		addStandardArmorRecipes(consumer, ItemRegistrar.GHOSTSOUL_INGOT, ItemRegistrar.GHOSTSOUL_HELMET, ItemRegistrar.GHOSTSOUL_CHESTPLATE, ItemRegistrar.GHOSTSOUL_LEGGINGS, ItemRegistrar.GHOSTSOUL_BOOTS);
+		addStandardArmorRecipes(consumer, ItemRegistrar.GLOWSTONE_CRYSTAL, ItemRegistrar.GLOWSTONE_CRYSTAL_HELMET, ItemRegistrar.GLOWSTONE_CRYSTAL_CHESTPLATE, ItemRegistrar.GLOWSTONE_CRYSTAL_LEGGINGS, ItemRegistrar.GLOWSTONE_CRYSTAL_BOOTS);
+	}
+
+	private void addStandardToolRecipes(Consumer<IFinishedRecipe> consumer, Item material, Item axe, Item hoe, Item pickaxe, Item shovel, Item sword)
+	{
+		ShapedRecipeBuilder.shapedRecipe(axe)
+		.patternLine("MM")
+		.patternLine("MS")
+		.patternLine(" S")
+		.key('M', material)
+		.key('S', Items.STICK)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
+		.build(consumer);
+		ShapedRecipeBuilder.shapedRecipe(hoe)
+		.patternLine("MM")
+		.patternLine(" S")
+		.patternLine(" S")
+		.key('M', material)
+		.key('S', Items.STICK)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
+		.build(consumer);
+		ShapedRecipeBuilder.shapedRecipe(pickaxe)
+		.patternLine("MMM")
+		.patternLine(" S ")
+		.patternLine(" S ")
+		.key('M', material)
+		.key('S', Items.STICK)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
+		.build(consumer);
+		ShapedRecipeBuilder.shapedRecipe(shovel)
+		.patternLine("M")
+		.patternLine("S")
+		.patternLine("S")
+		.key('M', material)
+		.key('S', Items.STICK)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
+		.build(consumer);
+		ShapedRecipeBuilder.shapedRecipe(sword)
+		.patternLine("M")
+		.patternLine("M")
+		.patternLine("S")
+		.key('M', material)
+		.key('S', Items.STICK)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
+		.build(consumer);
+	}
+
+	private void addStandardArmorRecipes(Consumer<IFinishedRecipe> consumer, Item material, Item helmet, Item chestplate, Item leggings, Item boots)
+	{
+		ShapedRecipeBuilder.shapedRecipe(helmet)
+		.patternLine("MMM")
+		.patternLine("M M")
+		.key('M', material)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
+		.build(consumer);
+		ShapedRecipeBuilder.shapedRecipe(chestplate)
+		.patternLine("M M")
+		.patternLine("MMM")
+		.patternLine("MMM")
+		.key('M', material)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
+		.build(consumer);
+		ShapedRecipeBuilder.shapedRecipe(leggings)
+		.patternLine("MMM")
+		.patternLine("M M")
+		.patternLine("M M")
+		.key('M', material)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
+		.build(consumer);
+		ShapedRecipeBuilder.shapedRecipe(boots)
+		.patternLine("M M")
+		.patternLine("M M")
+		.key('M', material)
+		.addCriterion("has_" + material.getRegistryName().getPath(), hasItem(material))
 		.build(consumer);
 	}
 
