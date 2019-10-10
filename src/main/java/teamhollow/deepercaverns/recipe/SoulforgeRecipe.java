0d47b3f079e306b70cbe6f69cb32c7d3518e89d4
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-10-10 17:07:49 +0200

    add soulforge processing recipes

--- src/main/java/teamhollow/deepercaverns/recipe/SoulforgeRecipe.java
+++ src/main/java/teamhollow/deepercaverns/recipe/SoulforgeRecipe.java
@@ -4,20 +4,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
 import teamhollow.deepercaverns.DCTags;
+import teamhollow.deepercaverns.DeeperCaverns;
+import teamhollow.deepercaverns.reg.BlockRegistrar;
+import teamhollow.deepercaverns.reg.ItemRegistrar;
 
 public class SoulforgeRecipe
 {
 	public static final Map<ResourceLocation,SoulforgeRecipe> RECIPES = new HashMap<>();
 
 	static {
+		register(new SoulforgeRecipe(ItemRegistrar.REFINED_SOUL, ItemRegistrar.PALE_GLASS_SHARDS, ItemRegistrar.GHOSTSOUL_INGOT).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "ghostsoul_ingot")));
+		register(new SoulforgeRecipe(BlockRegistrar.ONYX_ORE, BlockRegistrar.ONYX_ORE, new ItemStack(ItemRegistrar.ONYX_INGOT, 2)).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "onyx_ingot")));
+		register(new SoulforgeRecipe(ItemRegistrar.ONYX_INGOT, Items.QUARTZ, ItemRegistrar.CHALONITE_INGOT).setRegistryName(new ResourceLocation(DeeperCaverns.MODID, "chalonite_ingot")));
 	}
 
 	private ResourceLocation registryName;
-	private Ingredient left, right, fuel = Ingredient.fromTag(DCTags.Items.FUEL_SOULFORGE);
+	private Ingredient left, right;
+	private final Ingredient fuel = Ingredient.fromTag(DCTags.Items.FUEL_SOULFORGE);
 	private ItemStack output;
+
+	public SoulforgeRecipe(IItemProvider left, IItemProvider right, IItemProvider output)
+	{
+		this(Ingredient.fromItems(left), Ingredient.fromItems(right), new ItemStack(output));
+	}
+
+	public SoulforgeRecipe(IItemProvider left, IItemProvider right, ItemStack output)
+	{
+		this(Ingredient.fromItems(left), Ingredient.fromItems(right), output);
+	}
+
+	public SoulforgeRecipe(Ingredient left, Ingredient right, IItemProvider output)
+	{
+		this(left, right, new ItemStack(output));
+	}
 
 	public SoulforgeRecipe(Ingredient left, Ingredient right, ItemStack output)
 	{
@@ -57,7 +81,7 @@ public class SoulforgeRecipe
 		return fuel;
 	}
 
-	private static void register(SoulforgeRecipe recipe)
+	public static void register(SoulforgeRecipe recipe)
 	{
 		if(recipe.registryName == null)
 			throw new IllegalStateException("Soulforge recipe cannot have null registry name!");
