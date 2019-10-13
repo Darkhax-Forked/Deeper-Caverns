31ad71d40d5629d6f1e5064d46522e7906d66ca0
Mahtaran <mahtaran@amuzil.com>
2019-10-13 15:55:01 +0200

    Merge changes with `master`
    Signed-off-by: Mahtaran <mahtaran@amuzil.com>

--- src/main/java/teamhollow/deepercaverns/DeeperCaverns.java
+++ src/main/java/teamhollow/deepercaverns/DeeperCaverns.java
@@ -3,17 +3,22 @@ package teamhollow.deepercaverns;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import teamhollow.deepercaverns.reg.ItemRegistrar;
 
 @Mod(DeeperCaverns.MODID)
 public class DeeperCaverns {
 	public static final String MODID = "deepercaverns";
 	public static final String PREFIX = MODID + ":";
+	public static final Logger LOGGER = LogManager.getLogger();
+
 	private static DeeperCaverns instance;
+
 	public static final ItemGroup ITEM_GROUP = new ItemGroup(ItemGroup.GROUPS.length, MODID) {
 		@Override
-		public ItemStack createIcon()
-		{
+		public ItemStack createIcon() {
 			return new ItemStack(ItemRegistrar.ARCANE_SPAWN_EGG);
 		}
 	};
