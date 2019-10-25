6d063766e33ca4042afbd73f2b3dbdaf91d8b181
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-10-25 15:14:13 +0200

    tweak biome weights

--- src/main/java/teamhollow/deepercaverns/world/biome/biolayer/BiolayerBiomes.java
+++ src/main/java/teamhollow/deepercaverns/world/biome/biolayer/BiolayerBiomes.java
@@ -5,7 +5,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.ObjectHolder;
-
 import teamhollow.deepercaverns.DeeperCaverns;
 import teamhollow.deepercaverns.util.RegistryUtil;
 import teamhollow.deepercaverns.world.biome.SpawnEntry;
@@ -31,16 +30,16 @@ public class BiolayerBiomes {
 	public static void setup(FMLCommonSetupEvent event) {
 		// TODO figure out good weights
 		SpawnEntryGroups.SURFACE.add(
-						new SpawnEntry<>(BIOLAYER_HELL, 10),
-						new SpawnEntry<>(SOULSTONE_HILLS, 5),
-						new SpawnEntry<>(PALE_SAND_DESERT, 5),
-						new SpawnEntry<>(PALE_SAND_DESERT_HILLS, 1),
-						new SpawnEntry<>(PALE_SAND_DESERT_LAKES, 2)
-		);
+				new SpawnEntry<>(BIOLAYER_HELL, 6),
+				new SpawnEntry<>(SOULSTONE_HILLS, 4),
+				new SpawnEntry<>(PALE_SAND_DESERT, 5),
+				new SpawnEntry<>(PALE_SAND_DESERT_HILLS, 1),
+				new SpawnEntry<>(PALE_SAND_DESERT_LAKES, 2)
+				);
 
 		SpawnEntryGroups.SURFACE_HILLS.add(
-						new SpawnEntry<>(SOULSTONE_HILLS, 5),
-						new SpawnEntry<>(PALE_SAND_DESERT_HILLS, 3)
-		);
+				new SpawnEntry<>(SOULSTONE_HILLS, 5),
+				new SpawnEntry<>(PALE_SAND_DESERT_HILLS, 3)
+				);
 	}
 }
