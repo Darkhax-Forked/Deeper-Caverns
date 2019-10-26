6ac40b0f91835374a162c0a1bac435c705bc55b7
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-10-26 15:16:27 +0200

    disable soul essence removing effects from creative players

--- src/main/java/teamhollow/deepercaverns/block/SoulEssenceFluidBlock.java
+++ src/main/java/teamhollow/deepercaverns/block/SoulEssenceFluidBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -20,7 +21,7 @@ public class SoulEssenceFluidBlock extends FlowingFluidBlock
 	@Override
 	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity)
 	{
-		if(entity instanceof LivingEntity)
+		if(!(entity instanceof PlayerEntity && ((PlayerEntity)entity).isCreative()) && entity instanceof LivingEntity)
 			((LivingEntity)entity).clearActivePotions();
 	}
 }
